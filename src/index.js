a33bff8bf8f3679129f12c19255f56d028ba3353
J Resilient <j.resilient.1@gmail.com>
2021-07-23 17:31:21 -0400

    Add some code to test Asteroid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 console.log("webpack is working");
 const MovingObject = require("./moving_object.js");
+const Asteroid = require("./asteroid.js");
 
-// document.getEventListener("DOMContentLoaded", () => {
-//     const canvas = document.getElementById("game-canvas");
-//     const ctx = canvas.getContext("2d");
-//     // construct a GameView and call start on it
-// });
+document.addEventListener("DOMContentLoaded", (e) => {
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext("2d");
+    let x = new Asteroid([30, 30]);
+    x.draw(ctx);
+    x.move();
+    x.draw(ctx);
+    // construct a GameView and call start on it
+});
 
 window.MovingObject = MovingObject;
+window.Asteroid = Asteroid;
