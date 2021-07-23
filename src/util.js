8bdee5a5e6906fac7f5e6637558678aa520b2c00
J Resilient <j.resilient.1@gmail.com>
2021-07-22 21:10:47 -0400

    Add pseudocode comments

---
+++ src/util.js
@@ -0,0 +1,7 @@
+const Util = {
+    inherits(childclass, parentClass) {
+        // code to make the childclass inherit from the parent class
+        // then export a JS object containing it
+    }
+};
+module.exports = Util;
