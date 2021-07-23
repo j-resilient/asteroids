560dfdcfaded80868cd983c1018c827e8323e8b0
J Resilient <j.resilient.1@gmail.com>
2021-07-23 15:55:33 -0400

    Add #draw to MovingObject

--- src/moving_object.js
+++ src/moving_object.js
@@ -18,8 +18,10 @@ function MovingObject(optionsObj) {
 }
 
 MovingObject.prototype.draw = function (ctx) {
-    // draw a circle with radius at pos
-    // fill circle with color
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, false);
+    ctx.fill();
 }
 
 MovingObject.prototype.move = function() {
