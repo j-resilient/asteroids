2080712fd7d64dad41a4b3c3dceeab856ae63dfa
J Resilient <j.resilient.1@gmail.com>
2021-07-23 19:50:15 -0400

    Remove unnecessary console.log from MovingObject

--- src/moving_object.js
+++ src/moving_object.js
@@ -36,7 +36,6 @@ MovingObject.prototype.isCollidedWith = function (otherObject) {
     // is less than the sum of their radii
     let distance = Math.sqrt(((this.pos[0] - otherObject.pos[0]) ** 2) + ((this.pos[1] - otherObject.pos[1]) ** 2));
     let sum = this.radius + otherObject.radius;
-    console.log(`Distance: ${distance}, Sum: ${sum}`);
     return (distance < sum ? true : false);
 }
 
