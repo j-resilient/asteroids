13e7c4729aa88e1d270adef60ee4257e7fdf0107
J Resilient <j.resilient.1@gmail.com>
2021-07-22 21:19:45 -0400

    Add vector methods from the project specs

--- src/util.js
+++ src/util.js
@@ -2,6 +2,15 @@ const Util = {
     inherits(childclass, parentClass) {
         // code to make the childclass inherit from the parent class
         // then export a JS object containing it
+    },
+    // next two methods provided by appacademy for the project
+    randomVec(length) {
+        const deg = 2 * Math.PI * Math.random();
+        return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+    },
+    // Scale the length of a vector by the given amount.
+    scale(vec, m) {
+        return [vec[0] * m, vec[1] * m];
     }
 };
 module.exports = Util;
