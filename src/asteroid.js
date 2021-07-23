27dbe21cd620ab53ae9003d70f39df8b3e4f1f85
J Resilient <j.resilient.1@gmail.com>
2021-07-23 17:30:25 -0400

    Add constructor function to Asteroid

--- src/asteroid.js
+++ src/asteroid.js
@@ -1,11 +1,15 @@
 const Util = require("./util");
+const MovingObject = require("./moving_object");
 
 // should inherit from MovingObject
-function Asteroid() {
-    MovingObject.call(optionsObj);
-    // pick default color and radius; set them as properties
-    // specify the pos and get a random vector from the Util object
-    // pass it all in to the MovingObject constructor
+function Asteroid(pos) {
+    MovingObject.call(this, { 
+        pos: pos, 
+        vel: Util.randomVec(this.radius * 2), 
+        radius: 5, 
+        color: "#a9a9a9" 
+    });
 }
 
 Util.inherits(Asteroid, MovingObject);
+module.exports = Asteroid;
