fab22c32afb3ef53a04c29c170b426ee1996417a
J Resilient <j.resilient.1@gmail.com>
2021-07-23 18:53:17 -0400

    Add code to event listener call back in src/index.js to get a new GameView and start the game

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ console.log("webpack is working");
 const MovingObject = require("./moving_object.js");
 const Asteroid = require("./asteroid.js");
 const Game = require("./game.js");
+const GameView = require("./game_view.js");
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
-    // let x = new Game();
-    // x.draw(ctx);
-    // setInterval(() => {
-    //     x.moveObjects();
-    //     x.draw(ctx);
-
-    // }, 100);
-    // construct a GameView and call start on it
+    const newView = new GameView(ctx);
+    newView.start();
 });
 
 window.MovingObject = MovingObject;
 window.Asteroid = Asteroid;
 window.Game = Game;
+window.GameView = GameView;
