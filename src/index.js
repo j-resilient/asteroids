51f45a4b273981bfa652425b20651db3d4195eab
J Resilient <j.resilient.1@gmail.com>
2021-07-23 18:44:31 -0400

    Remove asteroid test code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 console.log("webpack is working");
 const MovingObject = require("./moving_object.js");
 const Asteroid = require("./asteroid.js");
+const Game = require("./game.js");
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
-    let x = new Asteroid([30, 30]);
-    x.draw(ctx);
-    x.move();
-    x.draw(ctx);
+    // let x = new Game();
+    // x.draw(ctx);
+    // setInterval(() => {
+    //     x.moveObjects();
+    //     x.draw(ctx);
+
+    // }, 100);
     // construct a GameView and call start on it
 });
 
 window.MovingObject = MovingObject;
 window.Asteroid = Asteroid;
+window.Game = Game;
