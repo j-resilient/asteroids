ba5ae5d7e2fc3716ede1ae2d128ab204b9946c27
J Resilient <j.resilient.1@gmail.com>
2021-07-22 21:25:37 -0400

    Add pseudocode in comments

---
+++ src/asteroid.js
@@ -0,0 +1,6 @@
+// should inherit from MovingObject
+function Asteroid() {
+    // pick default color and radius; set them as properties
+    // specify the pos and get a random vector from the Util object
+    // pass it all in to the MovingObject constructor
+}
---
+++ src/game.js
@@ -0,0 +1,19 @@
+// Holds all of the moving objects and the logic for moving them
+function Game() {
+    // define DIM_X, DIM_Y and NUM_ASTEROIDS
+    // create empty array asteroids
+    // call addAsteroids
+}
+Game.prototype.addAsteroids = function() {
+    // randomly place asteroids
+    // store in asteroids array
+}
+Game.prototype.randomPosition = function() {
+    // generate random positions
+}
+Game.prototype.draw = function(ctx) {
+    // call clearRect on ctx and call draw on every asteroid
+}
+Game.prototype.moveObjects = function() {
+    // call move on every asteroid
+}
