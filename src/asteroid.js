b1908d0a50ddc5fb5075ea5f6066a8d7ebe67ed9
J Resilient <j.resilient.1@gmail.com>
2021-07-23 17:11:20 -0400

    Add code so Asteroid inherits from MovingObject

--- src/asteroid.js
+++ src/asteroid.js
@@ -1,6 +1,11 @@
+const Util = require("./util");
+
 // should inherit from MovingObject
 function Asteroid() {
+    MovingObject.call(optionsObj);
     // pick default color and radius; set them as properties
     // specify the pos and get a random vector from the Util object
     // pass it all in to the MovingObject constructor
 }
+
+Util.inherits(Asteroid, MovingObject);
