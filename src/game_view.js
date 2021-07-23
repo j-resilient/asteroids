a728af199b931e93d87cb6cd8c893ec9ea7e871f
J Resilient <j.resilient.1@gmail.com>
2021-07-22 21:29:25 -0400

    Add pseudocode in comments

---
+++ src/game_view.js
@@ -0,0 +1,9 @@
+// keeps track of the canvas context, the game, and the ship
+// sets the interval to animate the game
+function GameView(ctx) {
+    // store a Game
+    // take in and store the drawing ctx
+}
+GameView.prototype.start = function() {
+    // call setInterval to call Game#moveObjects and Game#draw every 20ms
+}
