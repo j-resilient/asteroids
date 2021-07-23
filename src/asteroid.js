0250323cddcf207e8bd5e062f9e0e1c400f55721
J Resilient <j.resilient.1@gmail.com>
2021-07-23 19:08:03 -0400

    Change Asteroid constructor to take the current game object and pass it to MovingObject's constructor

--- src/asteroid.js
+++ src/asteroid.js
@@ -2,12 +2,13 @@ const Util = require("./util");
 const MovingObject = require("./moving_object");
 
 // should inherit from MovingObject
-function Asteroid(pos) {
+function Asteroid(pos, currentGame) {
     MovingObject.call(this, { 
         pos: pos, 
         vel: Util.randomVec(2), 
         radius: 5, 
-        color: "#a9a9a9" 
+        color: "#a9a9a9",
+        game: currentGame
     });
 }
 
