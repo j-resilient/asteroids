3309543982f53a527c3d743d6633bd9da0288185
J Resilient <j.resilient.1@gmail.com>
2021-07-23 15:50:32 -0400

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+console.log("webpack is working");
 const MovingObject = require("./moving_object.js");
 
-// add listener for DOMContentLoaded
-// in call back, 
-    // get canvas element and call getContext on it with 2d
-    // construct a GameView and call start on it
+// document.getEventListener("DOMContentLoaded", () => {
+//     const canvas = document.getElementById("game-canvas");
+//     const ctx = canvas.getContext("2d");
+//     // construct a GameView and call start on it
+// });
 
 window.MovingObject = MovingObject;
