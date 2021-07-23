723ca12cf7060430746d09861aa951f1b2ebe3b8
J Resilient <j.resilient.1@gmail.com>
2021-07-23 18:46:45 -0400

    Add constructor to GameView

--- src/game_view.js
+++ src/game_view.js
@@ -1,9 +1,11 @@
 // keeps track of the canvas context, the game, and the ship
 // sets the interval to animate the game
 function GameView(ctx) {
-    // store a Game
-    // take in and store the drawing ctx
+    this.game = new Game();
+    this.ctx = ctx;
 }
 GameView.prototype.start = function() {
     // call setInterval to call Game#moveObjects and Game#draw every 20ms
 }
+
+module.exports = GameView;
