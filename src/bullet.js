b603c5feccfd761467a6c45432fddfd5fb1c8dc0
J Resilient <j.resilient.1@gmail.com>
2021-07-24 19:58:43 -0400

    Add constructor to Bullet

--- src/bullet.js
+++ src/bullet.js
@@ -1,6 +1,14 @@
+const MovingObject = require("./moving_object");
+
 // Subclass of MovingObject
-function Bullet(vel) {
-    this.vel = vel;
+function Bullet(vel, pos, game) {
+    MovingObject.call({
+        vel: vel,
+        pos: pos,
+        game: game,
+        color: "#66FF00",
+        radius: 5
+    });
     // #66FF00
 
 }
