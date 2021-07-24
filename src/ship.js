8aff6228e74b4330f3fd8096e3911081a3c6e253
J Resilient <j.resilient.1@gmail.com>
2021-07-23 21:07:46 -0400

    Add Ship constructor

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,18 @@
-function Ship() {
+const MovingObject = require("./moving_object");
+const Util = require("./util");
+
+function Ship(currentGame, pos) {
     // define ship color, radius, and vel
+    MovingObject.call(this, {
+        pos: pos,
+        vel: 0,
+        radius: 5,
+        color: "#66FF00",
+        game: currentGame
+    });
 }
 // ship should inherit from MovingObject
+Util.inherits(Ship, MovingObject);
 
 Ship.prototype.relocate = function() {
     // reset ship's position to random location
