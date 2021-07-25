2d53e4c632aaac2f72e9b22b5941054faf5d2242
J Resilient <j.resilient.1@gmail.com>
2021-07-25 18:37:13 -0400

    Remove unnecessary code from index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log("webpack is working");
 const MovingObject = require("./moving_object.js");
 const Asteroid = require("./asteroid.js");
 const Game = require("./game.js");
@@ -9,8 +8,7 @@ const Bullet = require("./bullet.js");
 document.addEventListener("DOMContentLoaded", (e) => {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
-    const newView = new GameView(ctx);
-    newView.start();
+    new GameView(ctx).start();
 });
 
 window.MovingObject = MovingObject;
