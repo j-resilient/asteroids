a6447bad653aeaa6e6d31f618c0cccd152be0058
J Resilient <j.resilient.1@gmail.com>
2021-07-25 18:37:39 -0400

    Remove unnecessary comments from MovingObjects

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,14 +1,6 @@
 // base class for anything that moves
 // move(), draw(ctx), isCollidedWith(otherMovingObject)
 
-//  example options object:
-// {
-//   pos: [30, 30],
-//   vel: [10, 10],
-//   radius: 5,
-//   color: "#00FF00"
-// };
-
 function MovingObject(optionsObj) {
     // 2d velocity and position
     this.pos = optionsObj.pos;
