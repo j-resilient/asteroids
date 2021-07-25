dd43deff7fd306bfb58fdbac39fd7c2a075cae7d
J Resilient <j.resilient.1@gmail.com>
2021-07-25 18:05:40 -0400

    Change MovingObject so that move() only wraps wrappable objects: bullets disappear when they go off screen

--- src/moving_object.js
+++ src/moving_object.js
@@ -16,6 +16,7 @@ function MovingObject(optionsObj) {
     this.radius = optionsObj.radius;
     this.color = optionsObj.color;
     this.game = optionsObj.game;
+    this.isWrappable = true;
 }
 
 MovingObject.prototype.draw = function (ctx) {
@@ -26,10 +27,15 @@ MovingObject.prototype.draw = function (ctx) {
 }
 
 MovingObject.prototype.move = function() {
-    this.pos = this.game.wrap(this.pos);
+    if (this.game.isOutOfBounds(this.pos)) {
+        if (this.isWrappable) { 
+            this.pos = this.game.wrap(this.pos); 
+        } else {
+            this.game.remove(this);
+        }
+    }
     this.pos[0] += this.vel[0];
     this.pos[1] += this.vel[1];
-    // check if object is out of bounds and wrap if wrappable || remove
 }
 
 MovingObject.prototype.isCollidedWith = function (otherObject) {
