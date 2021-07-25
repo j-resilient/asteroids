b5f39713caaef7f5ce44d7d3e9e0586fd3965c36
J Resilient <j.resilient.1@gmail.com>
2021-07-24 21:45:26 -0400

    Change Bullet constructor to call velocity(dirX, dirY) so that the bullets move faster than the ship

--- src/bullet.js
+++ src/bullet.js
@@ -1,16 +1,25 @@
 const MovingObject = require("./moving_object");
+const Util = require("./util");
 
 // Subclass of MovingObject
-function Bullet(vel, pos, game) {
+function Bullet(dirX, dirY, x, y, game) {
     MovingObject.call(this, {
-        vel: vel,
-        pos: pos,
+        vel: this.velocity(dirX, dirY),
+        pos: [x, y],
         game: game,
         color: "#66FF00",
         radius: 5
     });
-    // #66FF00
+}
+Util.inherits(Bullet, MovingObject);
 
+Bullet.prototype.velocity = function(dirX, dirY) {
+    let velocity = [dirX, dirY];
+    if (dirX > 0) { velocity[0] = (dirX + 1) }
+    if (dirX < 0) { velocity[0] = (dirX - 1) }
+    if (dirY > 0) { velocity[1] = (dirY + 1) }
+    if (dirY < 0) { velocity[1] = (dirY - 1) }
+    return velocity;
 }
 
 module.exports = Bullet;
